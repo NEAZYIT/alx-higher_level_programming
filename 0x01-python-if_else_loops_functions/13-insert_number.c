03085c83ac6c279bf7b6d14eee342dbd0c9cbaa8
NEAZYIT <touihnazih28@gmail.com>
2023-06-06 14:00:27 -0700

    Implemented insertion of a node into a sorted singly linked list in C and fix betty in line 10 space required vefor the open (.ADD description for function insert_node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * insert_node - Inserts a number into a sorted singly linked list.
+ * @head: list head
+ * @number: number to store in the new node
+ * Return: pointer to the new node
+ */
+
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node, *current;
