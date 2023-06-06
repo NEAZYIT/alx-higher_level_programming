4ff9f32ed888a5372e7288d25b1a7efde3919e5a
NEAZYIT <touihnazih28@gmail.com>
2023-06-06 13:25:18 -0700

    Implement function to print string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(string):
         ascii_val = ord(char)
         if 97 <= ascii_val <= 122:
             ascii_val -= 32
-        print(chr(ascii_val), end="")
+        print("{}".format(chr(ascii_val)), end="")
     print()
