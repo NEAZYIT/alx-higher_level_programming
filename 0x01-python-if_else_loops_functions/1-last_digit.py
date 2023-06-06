0c390eb5dbce10da76b9fcb252a905a9dd06d827
NEAZYIT <touihnazih28@gmail.com>
2023-06-06 12:40:57 -0700

    Add code to print the last digit of a randomly generated number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 # Get the last digit of the number
-last_digit = abs(number) // 10
+last_digit = abs(number) % 10
 if number < 0:
     last_digit = -last_digit
 print("Last digit of", number, "is", last_digit, end=" ")
