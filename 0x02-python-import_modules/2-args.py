0f04a5985838b9b82f80bd31afb360a23b094a2c
NEAZYIT <touihnazih28@gmail.com>
2023-06-08 14:05:24 -0700

    Added program to import functions, perform calculations, and print results; created a program to print the number of arguments and their values from the command line using sys.argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-import sys
-def print_args():
-    """Prints the number of and the list of its argumets."""
-
-    num_args = len(sys.argv)
-    if num_args == 0:
-        print("0 arguments.")
-    else:
-        print(f"{num_args} arguments:")
-        for i in range(1, num_args + 1):
-            print(f"{i}: {sys.argv[i - 1]}")
-
 if __name__ == "__main__":
-    print_args()
+    import sys
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args > 1:
+        print("{} arguments:".format(num_args))
+        for i, arg in enumerate(args, start=1):
+            print("{}: {}".format(i, arg))
+
+    elif num_args == 0:
+        print("{} arguments.".format(num_args))
+
+    else:
+        print("{} arguments:".format(num_args))
+        print("{}: {}".format(num_args, args[0])
