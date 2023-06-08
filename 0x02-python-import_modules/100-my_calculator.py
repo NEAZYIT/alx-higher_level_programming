1153ba4a5c3081a47b70679fb55416fcbb7b9282
NEAZYIT <touihnazih28@gmail.com>
2023-06-08 14:55:08 -0700

    Added script to import functions from calculator_1.py and handle basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage : {} <a> <operator> <b>".format(argv[0]))
         sys.exit(1)
 
     a = int(sys.argv[1])
