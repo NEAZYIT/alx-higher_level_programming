22cf9fe6b77b2f3413a2c87e438488b7a695575e
NEAZYIT <touihnazih28@gmail.com>
2023-06-08 15:27:35 -0700

    Implemented Python function magic_calculation(a, b) to match the given bytecode.

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    add = __import__('magic_calculation_102').add
-    sub = __import__('magic_calculation_102').sub
+from magic_calculation_102 import add, sub
 
-    if a < b:
-        c = add(a ,b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-    else:
-        return sub(a, b)
+def magic_calculation(a, b):
+    return (add(add(a, b), sum(add(a, b) for i in range(4, 6)))) if a < b else sub(a, b)
