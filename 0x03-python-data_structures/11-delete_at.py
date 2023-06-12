17c87fcda7f3d01d81fda26aceb24b45e7dd1ffa
NEAZYIT <touihnazih28@gmail.com>
2023-06-12 11:32:14 -0700

    The delete_at function has been implemented to delete an item at a specific position in a list. It checks for negative or out-of-range indices and returns the original list unchanged in those cases. Otherwise, it creates a new list without the element at the specified index and returns it. The functionality has been validated using the provided test script 11-main.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    # Check if the index is negative or out of range
     if idx < 0 or idx >= len(my_list):
-        return (my_list) # Return the original list without any changes
-    # Check if the list is None
-    if my_list is None:
-        return (None) # Return None if the list is None
+        return (my_list)
 
-    del my_list[idx] # Delete the item at the specified index
-    return (my_list) # Return the modified list
+    if my_list is None:
+        return (None)
+
+    del my_list[idx]
+    return (my_list)
