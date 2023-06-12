c23ccb49556a57c0d711f3fe107c60d2334f9e9e
NEAZYIT <touihnazih28@gmail.com>
2023-06-12 14:15:42 -0700

    Fix palindrome check in linked list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,24 +6,24 @@
  * reverse_listint - reverses a linked list
  * @head: pointer to the first node in the list
  *
- * Return: pointer to the first node in the new list
+ * Return: pointer to the frist node in the new list
  */
 
 listint_t *reverse_listint(listint_t *head)
 {
-    listint_t *prev = NULL;
-    listint_t *current = head;
-    listint_t *next = NULL;
+	listint_t *prev = NULL;
+	listint_t *current = head;
+	listint_t *next = NULL;
 
-    while (current != NULL)
-    {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
 
-    return (prev);
+	return (prev);
 }
 
 /**
@@ -35,42 +35,37 @@ listint_t *reverse_listint(listint_t *head)
 
 int is_palindrome(listint_t **head)
 {
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-    listint_t *slow = *head;
-    listint_t *fast = *head;
-    listint_t *prev = NULL;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
 
-    // Find the middle of the linked list
-    while (fast && fast->next)
-    {
-        fast = fast->next->next;
-        prev = slow;
-        slow = slow->next;
-    }
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		prev = slow;
+		slow = slow->next;
+	}
 
-    // If the length of the list is odd, move slow one step forward
-    if (fast)
-        slow = slow->next;
+	if (fast)
+		slow = slow->next;
 
-    // Reverse the second half of the list
-    prev->next = reverse_listint(slow);
+	prev->next = reverse_listint(slow);
 
-    listint_t *first = *head;
-    listint_t *second = prev->next;
+	listint_t *first = *head;
+	listint_t *second = prev->next;
 
-    // Compare the first half and reversed second half
-    while (second)
-    {
-        if (first->n != second->n)
-            return (0);
-        first = first->next;
-        second = second->next;
-    }
+	while (second)
+	{
+		if (first->n != second->n)
+			return (0);
+		first = first->next;
+		second = second->next;
+	}
 
-    // Restore the original list by reversing the second half again
-    prev->next = reverse_listint(prev->next);
+	prev->next = reverse_listint(prev->next);
 
-    return (1);
+	return (1);
 }
