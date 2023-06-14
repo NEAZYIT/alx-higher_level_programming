73459ab29f65c2ab99d7db32be1ba931368e2710
NEAZYIT <touihnazih28@gmail.com>
2023-06-14 10:20:46 -0700

    The square_matrix_simple function creates a new matrix with the same size as the input matrix. It then iterates over each element of the input matrix, computes the square value using the ** operator, and assigns the result to the corresponding position in the new matrix. The original matrix is not modified. Finally, it returns the new matrix with the squared values.

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = [[0 for _ in row] for row in matrix]
 
-    for i in  range(len(matrix)):
+    for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             new_matrix[i][j] = matrix[i][j] ** 2
 
