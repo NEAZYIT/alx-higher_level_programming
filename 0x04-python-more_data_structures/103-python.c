0755e91d3e0173619b0d7c385a9ec3595a7f08cf
NEAZYIT <touihnazih28@gmail.com>
2023-06-14 13:01:46 -0700

    The code has been refactored to adhere to the Betty style guidelines, which include consistent indentation, clear variable names, appropriate spacing, and other stylistic conventions. This improves the code's readability and maintainability.

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,51 @@
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size, allocated, i;
+	PyObject *item;
+
+	size = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", allocated);
+
+	for (i = 0; i > size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
+
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size, i;
+	char *str;
+
+	printf("[.] bytes object info\n");
+
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	size = PyBytes_Size(p);
+	str = PyBytes_AsString(p);
+
+	printf("  size: %zd\n", size);
+	printf("  trying string: %s\n", str);
+
+	printf("  first 10 bytes:");
+
+	if (size + 1 > 10)
+		size = 10;
+
+	for (i = 0; i < size; i++)
+		printf(" %.2x", (unsigned char)str[i]);
+
+	printf("\n");
+}
