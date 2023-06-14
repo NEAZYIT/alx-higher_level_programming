a907db7f6aa7316983a7e17ed9c6f664ef057bdc
NEAZYIT <touihnazih28@gmail.com>
2023-06-14 12:28:11 -0700

    This commit adds the roman_to_int function, which converts a Roman numeral string into an integer. The function handles input validation and utilizes a dictionary to map Roman numeral characters to their corresponding integer values. The conversion algorithm iterates through the input string in reverse order, comparing each character's value to the previous one to determine the appropriate addition or subtraction operation. The function returns the final converted integer.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,20 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
+    if type(roman_string) == str:
+        sum_v = 0
+        num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+        for i in range(len(roman_string)):
+
+            if i == len(roman_string) - 1:
+                sum_v += num[roman_string[i]]
+
+            elif num[roman_string[i + 1]] <= num[roman_string[i]]:
+                sum_v += num[roman_string[i]]
+
+            else:
+                sum_v -= num[roman_string[i]]
+
+        return (sum_v)
+    else:
         return (0)
-
-    roman_numerals = {
-            'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50,
-            'C': 100, 'D' : 500, 'M' : 1000
-            }
-
-    result = 0
-    prev_value = 0
-
-    for char in reversed(roman_string):
-        value = roman_numerals.get(char, 0)
-        if value >= prev_value:
-            result += value
-        else:
-            result -= value
-        prev_value = value
-
-    return (result)
