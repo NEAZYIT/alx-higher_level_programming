8121ffcd67918bfb481df84de9aa4d948c197c4f
NEAZYIT <touihnazih28@gmail.com>
2023-06-15 06:56:54 -0700

    This commit refactors the print_python_list and print_python_bytes functions in the codebase. The prohibited macros/functions mentioned in the assignment prompt have been replaced with alternative approaches to achieve the desired functionality. The modifications ensure compliance with the restrictions while maintaining the intended behavior of the functions. Additionally, the code has been reviewed and improved for readability and adherence to best practices. Variable naming conventions have been followed, and unnecessary code comments and unused variables have been removed.

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,49 +1,71 @@
-#include <Python.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+
+/**
+ * print_python_list - Function that prints basic info about python objects
+ * @p: PyObject
+ */
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
 
 void print_python_list(PyObject *p)
 {
-    Py_ssize_t size, i;
+	int size, i, alloc;
+	PyVarObject *var = (PyVarObject *)p;
+	PyListObject *list = (PyListObject *)p;
+	const char *type;
 
-    size = PyList_Size(p);
+	size = var->ob_size;
+	alloc = list->allocated;
 
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
 
-    for (i = 0; i < size; i++)
-    {
-        PyObject *item = PyList_GetItem(p, i);
-        printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
-        Py_DECREF(item);
-    }
+	for (i = 0; i < size; i++)
+	{
+		type = list->ob_item[i]->ob_type->tp_name;
+		printf("Element %d: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list->ob_item[i]);
+	}
 }
+
+/**
+ * print_python_bytes - Fucntion that prints basic info of python bytes
+ * @p: Pyobject of byte CPython structure
+ */
 
 void print_python_bytes(PyObject *p)
 {
-    Py_ssize_t size, i;
-    char *str;
+	PyBytesObject *bytes = (PyBytesObject *)p;
+	unsigned char size, c;
 
-    printf("[.] bytes object info\n");
+	printf("[.] bytes object info\n");
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
 
-    if (!PyBytes_Check(p))
-    {
-        printf("  [ERROR] Invalid Bytes Object\n");
-        return;
-    }
+	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+	printf("  trying string: %s\n", bytes->ob_sval);
 
-    size = PyObject_Length(p);
-    str = PyBytes_AsString(p);
+	if (((PyVarObject *)p)->ob_size > 10)
+		size = 10;
+	else
+		size = ((PyVarObject *)p)->ob_size + 1;
 
-    printf("  size: %zd\n", size);
-    printf("  trying string: %s\n", str);
-
-    printf("  first 10 bytes:");
-
-    if (size + 1 > 10)
-        size = 10;
-
-    for (i = 0; i < size; i++)
-        printf(" %.2x", (unsigned char)str[i]);
-
-    printf("\n");
+	printf("  first %d bytes: ", size);
+	for (c = 0; c < size; c++)
+	{
+		printf("%02hhx", bytes->ob_sval[c]);
+		if (c == (size - 1))
+			printf("\n");
+		else
+			printf(" ");
+	}
 }
