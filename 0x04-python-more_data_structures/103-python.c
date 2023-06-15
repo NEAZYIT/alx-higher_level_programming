166f8289446d36f3f35af2276bc2f9ee7a7968d3
NEAZYIT <touihnazih28@gmail.com>
2023-06-15 06:54:11 -0700

    This commit refactors the print_python_list and print_python_bytes functions in the codebase. The prohibited macros/functions mentioned in the assignment prompt have been replaced with alternative approaches to achieve the desired functionality. The modifications ensure compliance with the restrictions while maintaining the intended behavior of the functions.

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,49 +3,47 @@
 
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size, allocated, i;
-	PyObject *item;
+    Py_ssize_t size, i;
 
-	size = PyList_Size(p);
-	allocated = ((PyListObject *)p)->allocated;
+    size = PyList_Size(p);
 
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", allocated);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %zd\n", size);
 
-	for (i = 0; i > size; i++)
-	{
-		item = PyList_GetItem(p, i);
-		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
-	}
+    for (i = 0; i < size; i++)
+    {
+        PyObject *item = PyList_GetItem(p, i);
+        printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+        Py_DECREF(item);
+    }
 }
 
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size, i;
-	char *str;
+    Py_ssize_t size, i;
+    char *str;
 
-	printf("[.] bytes object info\n");
+    printf("[.] bytes object info\n");
 
-	if (!PyBytes_Check(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
+    if (!PyBytes_Check(p))
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
 
-	size = PyBytes_Size(p);
-	str = PyBytes_AsString(p);
+    size = PyObject_Length(p);
+    str = PyBytes_AsString(p);
 
-	printf("  size: %zd\n", size);
-	printf("  trying string: %s\n", str);
+    printf("  size: %zd\n", size);
+    printf("  trying string: %s\n", str);
 
-	printf("  first 10 bytes:");
+    printf("  first 10 bytes:");
 
-	if (size + 1 > 10)
-		size = 10;
+    if (size + 1 > 10)
+        size = 10;
 
-	for (i = 0; i < size; i++)
-		printf(" %.2x", (unsigned char)str[i]);
+    for (i = 0; i < size; i++)
+        printf(" %.2x", (unsigned char)str[i]);
 
-	printf("\n");
+    printf("\n");
 }
