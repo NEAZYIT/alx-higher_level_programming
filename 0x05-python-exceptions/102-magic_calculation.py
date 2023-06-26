7d18672819bfe8ef4d1862891716cedf88edf3f2
NEAZYIT <touihnazih28@gmail.com>
2023-06-26 16:53:07 -0700

    Translate Python bytecode into magic_calculation function

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,13 +2,14 @@
 def magic_calculation(a, b):
     result = 0
 
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-            result += (a ** b) / i
+            else:
+                result += a ** b / i
         except Exception:
-            result = b + a
-            break
+            result = a + b
+            pass
 
     return (result)
