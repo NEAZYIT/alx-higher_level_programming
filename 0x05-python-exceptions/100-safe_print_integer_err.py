cfb28ac9a353ee852fe8810050d978333e8b343d
NEAZYIT <touihnazih28@gmail.com>
2023-06-26 16:45:52 -0700

    Fix bug in safe_print_integer_err function

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,12 @@
 
 import sys
 
+
 def safe_print_integer_err(value):
     try:
-        sys.stdout.write("{:d}\n".format(value))
-        return (True)
-    except Exception as e:
-        sys.stderr.write("Exception: {}\n".format(e))
+        print("{:d}".format(value))
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
         return (False)
+
+    return (True)
