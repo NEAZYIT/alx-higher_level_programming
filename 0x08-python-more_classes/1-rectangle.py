f877e853122e8fd97492bda9f9887e5140b281ed
NEAZYIT <touihnazih28@gmail.com>
2023-09-04 12:01:30 -0700

    Implemented Rectangle class with width and height attributes and added a brief docstring.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,75 +1,32 @@
 #!/usr/bin/python3
 
 class Rectangle:
-    """
-    This class defines a rectangle.
-
-    Attributes:
-        --width (int): The width of the rectangle.
-        --height (int): The height of the rectangle.
-    """
+    """ Defines a rectangle """
 
     def __init__(self, width=0, height=0):
-        """
-        Initializes a new Rectangle instance.
-
-        Args:
-            width (int, optional): The width of the rectangle (default is 0).
-            height (int, optional): The height of the rectangle (default is 0).
-        """
         self.__width = width
         self.__height = height
 
+    """ WIDTH """
     @property
     def width(self):
-        """
-        Getter method for the width attribute.
-
-        Returns:
-            int: The width of the rectangle.
-        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Setter method for the width attribute.
-
-        Args:
-            value (int): The new width value.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-        """
-        if not isinstance(value, int):
+        def width(self, value):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    """ HEIGHT """
     @property
     def height(self):
-        """
-        Getter method for the height attribute.
-
-        Returns:
-            int: The height of the rectangle.
-        """
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Setter method for the height attribute.
-
-        Args:
-            value (int): The new height value.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
