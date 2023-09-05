eaf8c43249480ca59fdb81c1849d17d72b84915f
NEAZYIT <touihnazih28@gmail.com>
2023-09-05 10:44:30 -0700

    update some docstring and fix pycodestyle

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,40 +1,19 @@
 #!/usr/bin/python3
-"""Defines a function that prints a name."""
+"""Defines a name-printing function."""
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Print the name in the format "My name is <first name> <last name>".
+    """Print a name.
 
     Args:
-        first_name (str): The first name.
-        last_name (str): The last name (optional).
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
 
     Raises:
-        TypeError: If either `first_name` or `last_name` is not a string.
-
-    Example:
-        >>> say_my_name("John", "Smith")
-        My name is John Smith
-
-        >>> say_my_name("Walter", "White")
-        My name is Walter White
-
-        >>> say_my_name("Bob")
-        My name is Bob
-
-        >>> say_my_name(12, "White")
-        Traceback (most recent call last):
-        TypeError: first_name must be a string or last_name must be a string
-
-        >>> say_my_name("John", 42)
-        Traceback (most recent call last):
-        TypeError: first_name must be a string or last_name must be a string
+        TypeError: If either `first_name` or `last_name` are not strings.
     """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
-
-    if last_name:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
-        print("My name is {}".format(first_name))
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
