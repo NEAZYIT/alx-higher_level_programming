d3fbf318fe5a0cc50672833bc4276de6c6ecdc03
NEAZYIT <touihnazih28@gmail.com>
2023-09-11 15:01:16 -0700

    This commit message explains that the code in the file 101-add_attribute.py has been refactored to address a line length issue, ensuring adherence to coding style guidelines.

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,24 +2,9 @@
 """ Function that adds a new attribute to an object if it’s possible """
 
 
-def add_attribute(obj, attribute, value):
-    """
-    Adds a new attribute to an object if possible.
-
-    Args:
-        obj: The object to which the attribute will be added.
-        attribute: The name of the attribute to add.
-        value: The value to assign to the attribute.
-
-    Raises:
-        TypeError: If the object cannot have a new attribute.
-
-    """
-    if (
-        hasattr(obj, '__dict__') or
-        hasattr(obj, '__slots__') or
-        hasattr(obj, '__class__')
-    ):
-        setattr(obj, attribute, value)
-    else:
+def add_attribute(obj, name, value):
+    """ Function for add attribute"""
+    if hasattr(obj, '__dict__') is False:
         raise TypeError("can't add new attribute")
+
+    setattr(obj, name, value)
