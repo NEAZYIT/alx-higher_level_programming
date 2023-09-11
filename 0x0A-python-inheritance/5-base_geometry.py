311e98c13dab27a2b60b7950be03abe550fc06c2
NEAZYIT <touihnazih28@gmail.com>
2023-09-11 14:13:46 -0700

    Added a docstring to the BaseGeometry class.

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" New class Geometry module """
+
+
+class BaseGeometry:
+    """
+    This is the BaseGeometry class.
+    It serves as the base class for geometry-related classes.
+    """
+    pass
