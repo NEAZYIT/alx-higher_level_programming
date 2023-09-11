492458bf770f48f98648d56de4d8e73c6791638b
NEAZYIT <touihnazih28@gmail.com>
2023-09-11 14:50:46 -0700

    Refactor Square class to validate size parameter, encapsulate it as private attribute, and improve string representation.

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,18 +4,20 @@ Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """ Class square inherist from rectangle """
-    def __init__(self, size: int) -> None:
-        self.__size = size
-        super().__init__(self._validate_size(size), self._validate_size(size))
+    """ Class square inheriting from rectangle """
 
-    @staticmethod
-    def _validate_size(size: int) -> int:
-        if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
-        if size <= 0:
-            raise ValueError("Size must be a positive integer")
-        return size
+    def __init__(self, size):
+        """ Initializes a Square instance """
+        super().__init__(size, size)
+        self.size = size
 
-    def __str__(self) -> str:
-        return f"[Square] {self.__size}/{self.__size}"
+    @property
+    def size(self):
+        """ Getter for size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Setter for size """
+        self.width = value
+        self.height = value
