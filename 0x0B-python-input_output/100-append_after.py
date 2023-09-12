8c3f768ac402985cc6f2462c38c81f8ba4feb39e
NEAZYIT <touihnazih28@gmail.com>
2023-09-12 11:37:33 -0700

    Added script 100-append_after.py that inserts a line of text after each line containing a specific string in a file.

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module 100-append_after.
+Inserts a line of text to a file,
+after each line containing a specific string.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ Inserts a line of text after each line containing a specific string """
+    lines = []
+    with open(filename, 'r') as file:
+        for line in file:
+            lines.append(line)
+            if search_string in line:
+                lines.append(new_string)
+    with open(filename, 'w') as file:
+        file.writelines(lines)
+
+
+if __name__ == "__main__":
+    append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
