15630d0e69a3ac8d34f1f8ec318680f2c95e0f1b
NEAZYIT <touihnazih28@gmail.com>
2023-09-12 11:14:10 -0700

    script to load, add, and save items. It loads a JSON file named 'add_item.json,' adds any command-line arguments to a Python list, and then saves the updated list back to 'add_item.json.' If the file doesn't exist initially, it creates one and populates it with the command-line arguments.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """ Load, add, save  """
 from sys import argv
-from 6-load_from_json_file import load_from_json_file
-from 5-save_to_json_file import save_to_json_file
+
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 """ Script that adds all arguments to a Python list,
 and then save them to a file """
 try:
-    add_items = load_from_json_file('add_item.json')
-    save_to_json_file(add_items + argv[1:], 'add_item.json')
+    add_i = load_from_json_file('add_item.json')
+    save_to_json_file(add_i + argv[1:], 'add_item.json')
 except Exception:
     save_to_json_file(argv[1:], 'add_item.json')
