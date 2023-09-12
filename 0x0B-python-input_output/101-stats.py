10e5caa1bb844a769a002968c14f55b40ea7fe92
NEAZYIT <touihnazih28@gmail.com>
2023-09-12 11:43:34 -0700

    Add a script that reads lines from stdin, computes metrics on the input lines, and prints statistics in the specified format. The input format is defined, and it calculates the total file size and the number of lines for each status code (200, 301, 400, 401, 403, 404, 405, 500). This script handles keyboard interruptions (CTRL + C) and prints statistics at regular intervals or when interrupted

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,71 +1,58 @@
 #!/usr/bin/python3
-"""
-Script that reads stdin line by line and computes metrics:
+"""Reads from standard input and computes metrics.
 
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size>
-Each 10 lines and after a keyboard interruption (CTRL + C),
-prints those statistics since the beginning:
-Total file size: File size: <total size>
-where <total size> is the sum of all previous lines (see input format above)
-Number of lines by status code:
-Possible status codes: 200, 301, 400, 401, 403, 404, 405, and 500
-If a status code doesn’t appear, don’t print anything for this status code.
-Format: <status code>: <number>
-Status codes should be printed in ascending order.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
 
 
-import sys
-import signal
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
 
-
-def print_statistics(total_size, status_codes):
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
     """
-    Print the statistics.
-    """
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
+if __name__ == "__main__":
+    import sys
 
-def signal_handler(sig, frame):
-    """
-    Handle the keyboard interruption (CTRL + C).
-    """
-    print_statistics(total_size, status_codes)
-    sys.exit(0)
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
-total_size = 0
-status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-line_count = 0
+            line = line.split()
 
-signal.signal(signal.SIGINT, signal_handler)
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
 
-try:
-    for line in sys.stdin:
-        line_count += 1
-        split_line = line.split()
-        if len(split_line) > 2:
-            status_code = split_line[-2]
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        total_size += int(split_line[-1])
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
 
-        if line_count % 10 == 0:
-            print_statistics(total_size, status_codes)
+        print_stats(size, status_codes)
 
-except KeyboardInterrupt:
-    print_statistics(total_size, status_codes)
-    sys.exit(0)
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
