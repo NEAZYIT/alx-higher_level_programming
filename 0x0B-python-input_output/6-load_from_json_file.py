3771d2fade29def2ff0a29ed4fd35bd148176c5b
NEAZYIT <touihnazih28@gmail.com>
2023-09-12 11:02:37 -0700

    Created the load_from_json_file function that loads a JSON file and deserializes its content into a Python object. If the file doesn't exist or contains invalid JSON, appropriate exceptions are raised, as demonstrated in the provided example.

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 """ Create object from a JSON file """
+
 import json
 
 
 def load_from_json_file(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
-        return (json.load(file))
+    """ Function that creates an Object from a “JSON file” """
+    try:
+        with open(filename, 'r') as f:
+            data = f.read()
+            return (json.loads(data))
+    except FileNotFoundError:
+        return (None)
+    except json.JSONDecodeError:
+        return (None)
