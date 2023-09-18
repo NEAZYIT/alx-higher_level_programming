3a2d127ee1ef563bc6752d1c0eab63daadf9a7b4
NEAZYIT <touihnazih28@gmail.com>
2023-09-18 11:06:19 -0700

    Refactor Rectangle class str method formatting to comply with PEP 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,6 +26,13 @@ class Rectangle(Base):
         for i in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
+    def __str__(self):
+        """Override the __str__ method"""
+        return (
+                f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+                f"{self.__width}/{self.__height}"
+                )
+
     @property
     def width(self):
         """Getter method for width"""
