0f3ea2de8162184136c8ce1c699e57375009669f
NEAZYIT <touihnazih28@gmail.com>
2023-09-18 12:14:09 -0700

    Added from_json_string static method to the Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,3 +32,10 @@ class Base:
         list_dicts = [obj.to_dictionary() for obj in list_objs]
         with open(filename, mode="w", encoding="utf-8") as file:
             file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Convert a JSON string to a list of dictionaries."""
+        if not json_string:
+            return []
+        return json.loads(json_string)
