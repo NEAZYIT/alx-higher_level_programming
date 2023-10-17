c0bd335cdb58cee4e02285d424450d1777b40a17
NEAZYIT <touihnazih28@gmail.com>
2023-10-17 10:39:42 -0700

    Optimized SQL query for calculating and listing average temperatures by city in descending order of temperature

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -2,5 +2,5 @@
 -- Display the average temperature in Fahrenheit by city, ordered by temperature in descending order
 SELECT city, AVG(value) AS avg_temp
 FROM 'temperatures'
-GROUP BY city
+GROUP BY 'city'
 ORDER BY avg_temp DESC;
