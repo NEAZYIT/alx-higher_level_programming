f15d20b7c56b43986bb8286af19727be22a5b042
NEAZYIT <touihnazih28@gmail.com>
2023-10-17 10:06:13 -0700

    This script will update the score of the record with the name Bob to 10 in the second_table

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,7 @@
+-- Update Bob's Score
+-- This script updates Bob's score to 10 in the "second_table" of the "hbtn_0c_0" database.
+
+-- Update Bob's score to 10.
+UPDATE second_table
+SET score = 10
+WHERE name = "Bob";
