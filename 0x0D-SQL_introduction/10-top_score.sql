ff432bbc2c0a56feba1765330ce0f4340253780b
NEAZYIT <touihnazih28@gmail.com>
2023-10-17 09:57:52 -0700

    This script will retrieve and display the records from the second_table with scores and names ordered by score in descending order.

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- List Records of second_table by Score
+-- This script lists all records of the "second_table" table in the "hbtn_0c_0" database.
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
