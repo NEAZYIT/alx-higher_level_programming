8a2f3fbc1c0a0324321ddaad9ed7bddf95846e95
NEAZYIT <touihnazih28@gmail.com>
2023-10-17 10:12:16 -0700

    This script will calculate the average score and display it with the column name average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- Calculate Score Average
+-- This script calculates the average score of all records in the "second_table" of the "hbtn_0c_0" database.
+
+-- Calculate the average score.
+SELECT AVG(score) AS average
+FROM second_table;
