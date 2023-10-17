c29d9ebce38c1cb6baccac42188432c8d7ac66af
NEAZYIT <touihnazih28@gmail.com>
2023-10-17 10:27:21 -0700

    Converted hbtn_0c_0 database, first_table, and name field to UTF8 (utf8mb4_unicode_ci)

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,10 +5,7 @@ USE hbtn_0c_0;
 -- This script converts the specified database, table, and field to UTF8.
 
 -- Set the database character set and collation to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE 'hbtn_0c_0' CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Set the character set and collation for the first_table and its name field.
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Verify the changes.
-SHOW CREATE TABLE first_table;
