d6b1b900c9574dc0a270caf19e4c86a9bf4113f1
NEAZYIT <touihnazih28@gmail.com>
2023-10-17 09:41:33 -0700

    This script will insert the new row with the specified values into the first_table, and it will be visible in the subsequent listing of table values.

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Insert New Row in first_table
+-- This script inserts a new row with id  = 89 and name = "Best School" into the table "first_table"
+
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
