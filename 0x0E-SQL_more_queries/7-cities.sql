269911ff39137bc2deead8a6a96a64235da505ad
NEAZYIT <touihnazih28@gmail.com>
2023-10-18 13:04:22 -0700

    Added SQL script to create the 'hbtn_0d_usa' database and the 'cities' table within it, following the specified structure, and ensuring it doesn't fail if they already exist

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,16 @@
+-- TASK: Create 'hbtn_0d_usa' database and 'cities' table with specified structure
+
+-- Create the 'hbtn_0d_usa' database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the 'hbtn_0d_usa' database
+USE hbtn_0d_usa;
+
+-- Create the 'cities' table if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
