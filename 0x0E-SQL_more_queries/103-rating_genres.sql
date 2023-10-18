10e7240c4be00c3f6edb50a01065ab61cecdcc9b
NEAZYIT <touihnazih28@gmail.com>
2023-10-18 14:08:59 -0700

    Updated SQL query to list genres from the 'hbtn_0d_tvshows_rate' database by their ratings, ordered by rating in descending order

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- Task: List genres from the hbtn_0d_tvshows_rate database by their ratings.
+-- Retrieve genre names and their total ratings, ordered by rating in descending order.
+SELECT `name`, SUM(`rate`) AS `rating`
+FROM `tv_genres` AS g
+INNER JOIN `tv_show_genres` AS s ON s.`genre_id` = g.`id`
+INNER JOIN `tv_show_ratings` AS r ON r.`show_id` = s.`show_id`
+GROUP BY `name`
+ORDER BY `rating` DESC;
