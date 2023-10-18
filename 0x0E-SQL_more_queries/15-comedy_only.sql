974a257e536971ba323dd267e5a6733fa0340e13
NEAZYIT <touihnazih28@gmail.com>
2023-10-18 13:47:40 -0700

    Updated SQL query to list comedy shows in the 'hbtn_0d_tvshows' database, sorted by show title in descending order

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
--- Task: List genres of the show "Dexter" from the hbtn_0d_tvshows database.
--- Retrieve genre names linked to the show "Dexter," sorted in ascending order by genre name.
-SELECT g.`name`
-FROM `tv_genres` AS g
-INNER JOIN `tv_show_genres` AS s ON g.`id` = s.`genre_id`
-INNER JOIN `tv_shows` AS t ON t.`id` = s.`show_id`
-WHERE t.`title` = "Dexter"
-ORDER BY g.`name`;
+-- Task: List comedy shows in the hbtn_0d_tvshows database.
+-- Retrieve show titles categorized as "Comedy," sorted by show title in descending order.
+SELECT t.`title`
+FROM `tv_shows` AS t
+INNER JOIN `tv_show_genres` AS s ON t.`id` = s.`show_id`
+INNER JOIN `tv_genres` AS g ON g.`id` = s.`genre_id`
+WHERE g.`name` = "Comedy"
+ORDER BY t.`title` DESC;
