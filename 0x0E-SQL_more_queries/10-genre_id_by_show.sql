351f24ab47a19fd2747f0f910353a2560401c36c
NEAZYIT <touihnazih28@gmail.com>
2023-10-18 13:17:19 -0700

    Added SQL script to list shows in the 'hbtn_0d_tvshows' database with at least one linked genre, sorted by title and genre ID, using an INNER JOIN

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,12 +1,7 @@
--- TASK: List all shows in the 'hbtn_0d_tvshows' database with at least one linked genre.
--- TASK: List all shows in the 'hbtn_0d_tvshows' database with at least one linked genre.
+-- Task: List TV shows with at least one linked genre
 
--- Use the 'hbtn_0d_tvshows' database
-USE hbtn_0d_tvshows;
-
-
--- List shows with at least one linked genre
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+-- Select TV show titles and their linked genre IDs
+SELECT s.title AS title, g.genre_id AS genre_id
+FROM tv_shows s
+INNER JOIN tv_show_genres g ON s.id = g.show_id
+ORDER BY s.title, g.genre_id.
