fb5ade086623d2190e6064c7ec3d5554634c5ccc
NEAZYIT <touihnazih28@gmail.com>
2023-10-18 13:05:36 -0700

    Added SQL script to list all cities in California from the 'hbtn_0d_usa' database, sorting them by city ID, without using the JOIN keyword

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,12 @@
+-- TASK: List all cities of California from the 'hbtn_0d_usa' database
+
+-- Use the 'hbtn_0d_usa' database
+USE hbtn_0d_usa;
+
+-- Find the id of California in the 'states' table
+SET @california_id = (SELECT id FROM states WHERE name = 'California');
+
+-- List all cities of California sorted by cities.id
+SELECT * FROM cities
+WHERE state_id = @california_id
+ORDER BY id;
