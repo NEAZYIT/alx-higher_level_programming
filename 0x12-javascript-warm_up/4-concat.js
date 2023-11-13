9d12ed99a6820929efabc88787718f81e4828715
NEAZYIT <touihnazih28@gmail.com>
2023-11-13 08:14:40 -0800

    Create a JavaScript script that prints two arguments in the format 'arg1 is arg2', handling cases where arguments are missing and defaulting to 'undefined'.

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the two arguments
+const arg1 = process.argv[2] || 'undefined';
+const arg2 = process.argv[3] || 'undefined';
+
+// Print the arguments in the desired format
+console.log(arg1 + ' is ' + arg2);
