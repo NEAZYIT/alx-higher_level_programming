ca42e07160c907146f42801c8e6f631ff09c740f
NEAZYIT <touihnazih28@gmail.com>
2023-11-13 13:21:05 -0800

    Create a JavaScript script that prints 'C is fun' a specified number of times or 'Missing number of occurrences' if the argument is not a valid positive number.

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
-// Get the number of times to print "C is fun" from the command line argument
-const x = parseInt(process.argv[2]);
-
-// Check if x is a valid number
-if (!isNaN(x) && x > 0) {
-  for (let i = 0; i < x; i++) {
+const x = process.argv[2];
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < Number(x); i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
