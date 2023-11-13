d00b55ab000a2b32cc1efa98b9d30b9a52065bc0
NEAZYIT <touihnazih28@gmail.com>
2023-11-13 14:33:20 -0800

    Updated the script to include a function named incr within myObject. This function increments the value property of myObject by 1 each time it’s called. The updated myObject is printed after each increment to show the updated value.

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,20 +1,14 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
   value: 12,
-  incr: function () {
-    this.value++;
-  }
+  incr: function () { this.value++; }
 };
 
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
