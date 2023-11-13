331031b23cc7ed42f48830531254f20726f51b5e
NEAZYIT <touihnazih28@gmail.com>
2023-11-13 14:01:22 -0800

    Implemented a script that exports a function named add which returns the sum of two integers. The function is made visible from outside the script using exports.add = add;, allowing it to be imported by other scripts. The script does not use the var keyword; instead, it uses function to declare the function.

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// This script exports a function that adds two integers
+
+// Define the add function
+function add (a, b) {
+  return a + b;
+}
+
+// Export the add function
+exports.add = add;
