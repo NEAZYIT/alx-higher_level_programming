415d266006bc7dac51aa84a0bfdd8d8390419c23
NEAZYIT <touihnazih28@gmail.com>
2023-11-14 14:42:37 -0800

    Implement file concatenation script

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,24 +1,12 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const args = process.argv.slice(2);
 
-if (args.length !== 3) {
-  console.error('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
 
-const [fileA, fileB, fileC] = args;
+const dataA = fs.readFileSync(fileA, 'utf8');
+const dataB = fs.readFileSync(fileB, 'utf8');
 
-try {
-  const contentA = fs.readFileSync(fileA, 'utf-8');
-  const contentB = fs.readFileSync(fileB, 'utf-8');
-
-  const concatenatedContent = contentA + contentB;
-
-  fs.writeFileSync(fileC, concatenatedContent);
-
-  console.log(`The content of ${fileA} and ${fileB} has been concatenated and saved to ${fileC}`);
-} catch (err) {
-  console.error('Error:', err.message);
-  process.exit(1);
-}
+fs.writeFileSync(fileC, dataA + dataB);
