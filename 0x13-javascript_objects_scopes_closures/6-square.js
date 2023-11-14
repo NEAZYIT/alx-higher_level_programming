5a66def20f943e6b336f8c41363d98e0ac03fbca
NEAZYIT <touihnazih28@gmail.com>
2023-11-14 14:14:16 -0800

    Extend Square class, add charPrint method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,8 @@
 const Square5 = require('./5-square');
 
 class Square extends Square5 {
-  constructor (size) {
-    // Call the constructor of the parent class (Square5) using super()
-    super(size);
-  }
-
   charPrint (c) {
-    // If c is undefined, use the character 'X'; otherwise, use the specified character
     const char = c || 'X';
-
-    // Print the square using the specified character
     for (let i = 0; i < this.height; i++) {
       console.log(char.repeat(this.width));
     }
