a223e5169408b9e92f85690523b78ade9e005a11
NEAZYIT <touihnazih28@gmail.com>
2023-11-17 13:34:49 -0800

    This commit introduces a script that uses SQLAlchemy to insert a new State object with the name 'Louisiana' into a specified MySQL database. The script takes three command-line arguments: MySQL username, MySQL password, and database name. It connects to a MySQL server running on localhost at port 3306, creates a new State object, adds it to the session, and commits the changes to the database. After the new state is added, it prints the id of the new state.

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # setup engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name))
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Create a new State object
+    new_state = State(name="Louisiana")
+
+    # Add the new state to the session
+    session.add(new_state)
+
+    # Commit the changes to the database
+    session.commit()
+
+    # Print the id of the new state
+    print(new_state.id)
