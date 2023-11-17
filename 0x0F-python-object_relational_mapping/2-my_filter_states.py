42ee6619b9cbe079c3c2949033df3fde79110ed3
NEAZYIT <touihnazih28@gmail.com>
2023-11-17 12:36:19 -0800

    Added Python script to connect to a MySQL database and retrieve state information based on user input. The script accepts MySQL username, password, database name, and a state name as command-line arguments. It connects to the local MySQL server, queries the 'states' table to fetch the state information matching the provided name, and displays the results sorted by ID in ascending order.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,14 +6,39 @@ Lists states from hbtn_0e_0_usa where name matches the argument
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s\
-                 ORDER BY id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
+
+def filter_states():
+    # Extracting command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connecting to MySQL database
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    # Creating a cursor object
+    cursor = db.cursor()
+
+    # SQL query to select states by name and order by ID
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+    data = cursor.fetchall()
+
+    # Displaying the fetched data
+    for row in data:
         print(row)
-    cur.close()
+
+    # Closing cursor and database connection
+    cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    filter_states()
