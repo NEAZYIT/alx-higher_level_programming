16d455757058621936f82b156b85c24338ac340b
NEAZYIT <touihnazih28@gmail.com>
2023-11-17 13:41:26 -0800

    This commit introduces a script that uses SQLAlchemy to delete all State objects from a specified MySQL database that contain the letter 'a'. The script takes three command-line arguments: MySQL username, MySQL password, and database name. It connects to a MySQL server running on localhost at port 3306, queries the 'states' table for states containing the letter 'a', deletes them, and commits the changes to the database.

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script that deletes all State objects with a name containing the letter a
+from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # setup engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name))
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Query the database for states containing the letter 'a'
+    states = session.query(State).filter(State.name.contains('a')).all()
+
+    # Delete the states
+    for state in states:
+        session.delete(state)
+
+    # Commit the changes to the database
+    session.commit()
