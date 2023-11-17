6d4f21cfcc57a8fa4eddc591fc1a0d61e876b3bf
NEAZYIT <touihnazih28@gmail.com>
2023-11-17 13:38:11 -0800

    This commit introduces a script that uses SQLAlchemy to update the name of a State object in a specified MySQL database. The script takes three command-line arguments: MySQL username, MySQL password, and database name. It connects to a MySQL server running on localhost at port 3306, queries the 'states' table for the state with id = 2, changes its name to 'New Mexico', and commits the changes to the database.

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script that changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # setup engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name))
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Query the database for the state with id = 2
+    state = session.query(State).filter_by(id=2).first()
+
+    # Change the name of the state
+    if state is not None:
+        state.name = "New Mexico"
+
+    # Commit the changes to the database
+    session.commit()
