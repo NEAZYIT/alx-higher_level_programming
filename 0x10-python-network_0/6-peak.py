2a0ce65bd83a4f024e813664394ef914e410fa6b
NEAZYIT <touihnazih28@gmail.com>
2023-11-30 07:36:07 -0800

    Implement Python function find_peak to locate a peak in an unsorted list of integers without importing any modules.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,11 +5,21 @@ Finds the peak value in a list of integers using binary search algorithm.
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers"""
+    """Find a peak in a list of unsorted integers"""
     if not list_of_integers:
         return None
+
     length = len(list_of_integers)
-    for i in range(length - 1):
-        if list_of_integers[i] > list_of_integers[i + 1]:
-            return list_of_integers[i]
-    return list_of_integers[length - 1]
+    if length == 1:
+        return list_of_integers[0]
+
+    mid = length // 2
+    if ((mid == length - 1 or
+         list_of_integers[mid] >= list_of_integers[mid + 1]) and
+        (mid == 0 or
+         list_of_integers[mid] >= list_of_integers[mid - 1])):
+        return list_of_integers[mid]
+    elif (mid != length - 1 and
+          list_of_integers[mid + 1] > list_of_integers[mid]):
+        return find_peak(list_of_integers[mid:])
+    return find_peak(list_of_integers[:mid])
