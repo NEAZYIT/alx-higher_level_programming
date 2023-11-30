39fa6d69928abbe52f431372b1bf1b38b81bb5e2
NEAZYIT <touihnazih28@gmail.com>
2023-11-30 13:26:15 -0800

    Add Bash script for sending JSON POST request to a specified URL with content from a file. Handles validation for valid JSON content before sending the request.

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a specified URL using content from a file
-[ -z "$2" ] && echo "Usage: $0 <URL> <JSON_FILE>" && exit 1
-jq . "$2" &> /dev/null || { echo "Not a valid JSON"; exit 1; }
 curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
