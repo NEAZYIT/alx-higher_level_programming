e73ead3e0e20e6e9cb221bbb2cd1be8338712a5e
NEAZYIT <touihnazih28@gmail.com>
2024-01-03 08:02:10 -0800

    Added Node.js script to write specified string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf8', function(err) {
-    if (err) {
-        console.log(err);
-    }
+fs.writeFile(filePath, content, 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
