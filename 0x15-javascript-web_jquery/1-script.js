e9a7b01710136e3bfa809ec9fc2d69ef924ae268
NEAZYIT <touihnazih28@gmail.com>
2024-01-13 06:33:48 -0800

    Fix '$ is not defined' issue in 1-script.js by adding global comment for jQuery.

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+/* global $ */
+// Ensure the DOM is ready before executing the script
+$(document).ready(function () {
+  // Use jQuery to select the <header> element and update its text color to red
+  $('header').css('color', '#FF0000');
+});
