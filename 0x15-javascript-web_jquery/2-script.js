d8443d7865510e1e377c72e54339215508cf2301
NEAZYIT <touihnazih28@gmail.com>
2024-01-13 06:36:54 -0800

    This script uses jQuery to select an HTML element with the id 'red_header'. It then attaches a click event handler to this element. When the 'red_header' element is clicked, the script uses jQuery to select the 'header' element and changes its text color to red (#FF0000). In summary, clicking on the element with id 'red_header' will change the text color of the 'header' element to red.

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Trigger color change on clicking the element with id 'red_header'
+$('#red_header').on('click', function () {
+    // Update the text color of the <header> element to red
+    $('header').css('color', '#FF0000');
+});
