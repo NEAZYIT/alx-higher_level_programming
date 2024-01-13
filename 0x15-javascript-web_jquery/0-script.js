75a8e774401ede67b0a3e14a0e8e4f8a360b9e08
NEAZYIT <touihnazih28@gmail.com>
2024-01-13 06:27:43 -0800

    Update text color of <header> element to red (#FF0000)

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Wait for the DOM to be fully loaded before manipulating it
+  const headerElement = document.querySelector('header');
+
+  // Update the text color to red (#FF0000)
+  headerElement.style.color = '#FF0000';
+});
